3adff171977914deada0f99a16f78af292096a03
Giuseppe Sucameli <sucameli@netresults.it>
2022-02-22 13:22:50 +0100

    add script to start SNX VPN

---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CONFIGENVFILE="${1:-config.env}"
+IMAGENAME=snx-checkpoint-vpn
+CONTAINERNAME="${2:-snx-vpn}"
+
+function help() {
+	echo "Usage: $(basename "$0") <CONFIG_ENVFILE> [<CONTAINER_NAME>]
+
+where:
+CONFIG_ENVFILE  VPN configuration, see config.env.sample (default: config.env)
+CONTAINER_NAME  name to be assigned to the container (default: snx-vpn)" >&2
+}
+
+function cleanup() {
+	echo
+	echo "Stopping $CONTAINERNAME..."
+
+	docker stop "$CONTAINERNAME"
+	docker rm "$CONTAINERNAME"
+}
+
+if [ ! -e "$CONFIGENVFILE" ] ; then
+	echo "File $CONFIGENVFILE not found..." >&2
+	help && exit 1
+fi
+
+if docker ps | grep "$CONTAINERNAME" &> /dev/null ; then
+	echo "Container $CONTAINERNAME is already running" >&2
+	exit 2
+fi
+
+# perform cleanup on exit
+trap "cleanup" EXIT
+
+echo "$CONTAINERNAME started..."
+echo
+
+CONTAINERID="$(docker run --name "$CONTAINERNAME" --cap-add=ALL --net=host -v /lib/modules:/lib/modules --env-file "$CONFIGENVFILE" -t -d "$IMAGENAME")"
+docker attach "$CONTAINERID"
